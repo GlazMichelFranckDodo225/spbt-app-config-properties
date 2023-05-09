01da098b650574f63596ceb5d9c8113d6ca0ba47
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-09 15:53:57 +0200

    Project initialization

---
+++ src/main/java/com/dgmf/spbtappconfigproperties/SpbtAppConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.dgmf.spbtappconfigproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtAppConfigPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtAppConfigPropertiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgmf/spbtappconfigproperties/SpbtAppConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.spbtappconfigproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtAppConfigPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
