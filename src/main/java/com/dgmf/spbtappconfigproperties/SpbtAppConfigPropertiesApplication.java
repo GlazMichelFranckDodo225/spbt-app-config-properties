89f915b225917e2a735917ca70b273aeb8118c42
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-09 16:06:35 +0200

    Added the "app.version=1.0.1" property in the application.properties file and created the AppInitializer class

--- src/main/java/com/dgmf/spbtappconfigproperties/SpbtAppConfigPropertiesApplication.java
+++ src/main/java/com/dgmf/spbtappconfigproperties/SpbtAppConfigPropertiesApplication.java
@@ -1,7 +1,9 @@
 package com.dgmf.spbtappconfigproperties;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpbtAppConfigPropertiesApplication {
@@ -10,4 +12,16 @@ public class SpbtAppConfigPropertiesApplication {
 		SpringApplication.run(SpbtAppConfigPropertiesApplication.class, args);
 	}
 
+}
+
+/* Dans une application Spring Boot, si on veut exécuter quoi que soit au démarrage
+de l'application, on peut créer un "initializer" comme ci-dessous
+*/
+@Component
+class AppInitializer implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
