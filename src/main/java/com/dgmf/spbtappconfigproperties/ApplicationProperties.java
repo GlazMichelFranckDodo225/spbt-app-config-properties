62d386db73a5a6824ff665981dd2b6d6d598be90
FranckDodo91 <devlogappdgmf@gmail.com>
2023-05-10 11:49:43 +0200

    Added @Validated annotation on ApplicationProperties class and proper annotations on each attribute of inner class FtpProperties

--- src/main/java/com/dgmf/spbtappconfigproperties/ApplicationProperties.java
+++ src/main/java/com/dgmf/spbtappconfigproperties/ApplicationProperties.java
@@ -1,10 +1,14 @@
 package com.dgmf.spbtappconfigproperties;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 // Toutes les propriétés commençant par le préfixe "app"
 // seront liées aux variables de la classe ci-dessous
 @ConfigurationProperties(prefix = "app")
+@Validated
 public class ApplicationProperties {
     private String version;
     // private FtpProperties ftp = new FtpProperties(); ==> si on n'utilise
@@ -36,9 +40,13 @@ public class ApplicationProperties {
     }
 
     public static class FtpProperties {
+        @NotEmpty
         private String host;
+        @Positive
         private String port;
+        @NotEmpty
         private String username;
+        @NotEmpty
         private String password;
 
         public String getHost() {
